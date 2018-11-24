0bd9892933e759106b87ae2e9277e36a606ac4b0
Edward Heath <e42291+github@gmail.com>
2018-11-24 14:13:11 -0800

    Complete repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+	if(times < 0) return 'ERROR';
 
+	let repeated = "";
+
+	for(let i = 0; i < times; i++) {
+		repeated += str;
+	}
+	
+	return repeated;
 }
 
 module.exports = repeatString
