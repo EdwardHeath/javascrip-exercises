21f2ed4920fb20473e1b13563b0357b8b09dc328
Edward Heath <e42291+github@gmail.com>
2018-11-24 15:26:05 -0800

    Complete fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+	if(num < 0) return "OOPS";
+	if(num == 1 || num == 2) return 1;
 
+	let fib1 = 1;
+	let fib2 = 1;
+	let fib3 = 0;
+
+	for(let i = 2; i < num; i++) {
+		fib3 = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+
+	return fib2;
 }
 
 module.exports = fibonacci
