d1bcd08551df3817c7f279dd7d1aefc9a463008b
Edward Heath <e42291+github@gmail.com>
2018-11-25 18:19:23 -0800

    Complete removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
+	let arr = arguments[0];
+	let remove = Array.prototype.slice.call(arguments).slice(1);
 
+	remove.forEach(function(removed) {
+		arr = arr.filter(function(entry) {
+			return removed != entry;
+		});
+	});
+
+	return arr;
 }
 
 module.exports = removeFromArray
