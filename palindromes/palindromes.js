0a90a0c978d8b7a68876c5c397e7bdd84716a35e
Edward Heath <e42291+github@gmail.com>
2018-12-01 11:20:47 -0800

    Complete palindrome

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
+const palindromes = function(palindrome) {
+	let word = palindrome.toLowerCase().match(/\w+/g).join('');
 
+	for(let i = 0; i < (word.length) / 2; i++) {
+		if(word[i] !== word[word.length - i - 1]) {
+			return false;
+		}
+	}
+	return true;
 }
 
 module.exports = palindromes
