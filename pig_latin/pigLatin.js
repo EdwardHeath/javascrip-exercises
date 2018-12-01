2f6faeed10a379a43004ee5223607116e53f9848
Edward Heath <e42291+github@gmail.com>
2018-12-01 12:21:21 -0800

    Complete pigLatin

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,5 +1,25 @@
-function translate() {
-	// body...
+function translate(toTranslate) {
+	let consonants = [];
+	let words = toTranslate.split(' ');
+	let translated = [];
+
+	words.forEach((word) => {
+		consonants = [];
+		
+		for(let i = 0; i < word.length; i++) {
+			if(word[i] === 'u' && consonants[consonants.length - 1] === 'q') {
+				translated += word.slice(++i) + consonants.join('') + "uay ";
+				break;
+			} else if(!word[i].match(/[aeiou]/)) {
+				consonants.push(word[i]);
+			} else {
+				translated += word.slice(i) + consonants.join('') + "ay ";
+				break;
+			}
+		}
+	});
+
+	return translated.trim();
 }
 
 
